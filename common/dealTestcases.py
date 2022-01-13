11236a37f3c39d0e7809671f7ce0f7a8c99a19d5
姜文埼煜 <1779999@qq.com>
2022-01-14 00:51:35 +0800

    添加解析har

--- common/dealTestcases.py
+++ common/dealTestcases.py
@@ -2,28 +2,26 @@ import os
 from string import Template
 from common.yaml_util import YamlUtil
 import json
-
+import pymysql
 
 class DealTest:
+    def getCases(self, host, user, password, database):
+        con = pymysql.connect(host=host, user=user, password=password, database=database)
+        cur = con.cursor()
+        sql = ''' select s.jsonObj from product_test_functions_detail s
+                where s.mainFunctionId = 1
+        '''
+        cur.execute(sql)
+        data = cur.fetchall()
+        cur.close()
+        con.close()
+        t1 = list()
+        for i in data:
+            t1.append(json.loads(i[0]))
 
-    def getTestcases(self, data, **kwargs):
-        if len(kwargs) == 0:
-            data = Template(json.dumps(data)).safe_substitute(os.environ)
-        data = json.loads(data)
-        return data
+        return t1
 
 
-
-    def get(self):
-        os.environ['name'] = '测试'
-        os.environ['fuck'] = '我草'
-        t = {'a':{'b':123}}
-        y  = YamlUtil().read_yaml('test.yml')
-        print(t[y['step'][0]['temp'][0]])
-        print(self.getTestcases(y))
-
-
-DealTest().get()
 
 
 
--- testFunctions/test_basicQuery.py
+++ testFunctions/test_basicQuery.py
@@ -3,45 +3,68 @@ import json
 import allure
 from allure_commons._allure import Dynamic
 
+from common.dealTestcases import DealTest
 from common.requestPackage import RequestSend
 from common.yaml_util import YamlUtil
 
-@allure.feature('商品基础-品类管理')
+
+def getInfo():
+    data = YamlUtil().read_yaml('envConfig.yml', './configs/')['fangzhen']
+    temp = dict()
+    temp['host'] = data['db_link']
+    temp['user'] = data['db_user']
+    temp['password'] = data['db_pass']
+    temp['database'] = data['db_database']
+    return temp
+
+
+
+@allure.feature('基础接口探活测试')
 class TestBasicQuery:
 
     requests = RequestSend()
+    host = ''
+    token = ''
+    db_host = ''
+    db_user = ''
+    db_pass = ''
+    db_database = ''
 
-    # @allure.story('简简单单')
-    @pytest.mark.parametrize('info', YamlUtil().read_yaml('修改分类.yml')['teststeps'])
+    @allure.story('获取环境信息')
+    def test_getEnv(self):
+        data = YamlUtil().read_yaml('envConfig.yml', './configs/')['fangzhen']
+        TestBasicQuery.host = data['host']
+        TestBasicQuery.token = data['token']
+        TestBasicQuery.db_host = data['db_link']
+        TestBasicQuery.db_user = data['db_user']
+        TestBasicQuery.db_pass = data['db_pass']
+        TestBasicQuery.db_database = data['db_database']
+
+
+
+    # @pytest.mark.parametrize('info', YamlUtil().read_yaml('修改分类.yml')['teststeps'])
+    @allure.story('基础接口探活测试')
+    @pytest.mark.parametrize('info', DealTest().getCases(**getInfo()))
     def test_query(self, info):
-        '''查询、修改、审批分类'''
-        Dynamic.title(info['title'])
 
-        method = info['request']['method'].lower()
-        url = info['request']['url']
-        header = info['request']['headers']
-        header['cookie'] = 'uc_token=e957c609b1554018ae97fe1dc86e9cf1'
-        data = None
-        param = None
-        for key in info['request']:
-            if key == 'json':
-                data = json.dumps(info['request']['json'])
-            elif key == 'data':
-                data = info['request']['data']
-            elif key == 'params':
-                param = info['request']['params']
+        '''查询、修改、审批分类'''
+        Dynamic.title(info['name'])
+
+        method = info['method'].lower()
+        url = TestBasicQuery.host + info['path']
+        header = info['header']
+        header['cookie'] = 'uc_token={}'.format(TestBasicQuery.token)
+        data = info['data']
+        param = info['param']
+
+        print(info['name'], method, url, TestBasicQuery.host)
 
         with allure.step('请求地址:{}\n请求参数:{}\n{}\n'.format(url, data, param)):
             res = self.requests.request(method, url, header, data = data, params = param)
 
         with allure.step('返回参数:{}'.format(res.text)):
             self.requests.myLog.debug('返回值: {}'.format(res.text))
-            for val in info['validate']:
-                for k in val:
-                    if k == 'eq':
-                        assert res.json()[val[k][0]] == val[k][1]
-                    elif k == 'in':
-                        assert val[k][0] in res.text
+            assert info['validate'] == res.text
 
 
     # @pytest.mark.parametrize('info', YamlUtil().read_yaml('test.yml'))
