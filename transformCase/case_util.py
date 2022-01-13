35821711dc7601f938ecdca88a92422528f6b93d
姜文埼煜 <1779999@qq.com>
2022-01-13 21:53:46 +0800

    添加解析har

--- transformCase/case_util.py
+++ transformCase/case_util.py
@@ -1,5 +1,7 @@
+#encoding=utf-8
 import json
-import jsonpath
+import requests
+# import jsonpath
 
 
 
@@ -10,16 +12,34 @@ class CaseUtil:
 
 
     def harToJson(self, filename):
-        pass
+        with open(filename, 'r', encoding='utf-8-sig') as f:
+            data = json.loads(f.read())
+
+        for obj in data['log']['entries']:
+            url = obj['request']['url']
+            path = ''
+            for i in url.split('?')[0].split('/')[3:]:
+                path += '/' + i
+            print(path)
+
+            method = obj['request']['method']
+            req_data = obj['request']['postData']
+            param = dict()
+            for i in obj['request']['queryString']:
+                param[i['name']] = i['value']
+
+            header = dict()
+            for i in obj['request']['headers']:
+                if i['name'] == 'Accept' or i['name'] == 'Content-Type':
+                    header[i['name']] = i['value']
+
+            res = json.loads(obj['response']['content']['text'])
+
+
 
 
     def addCase(self):
         pass
 
 
-
-a = {'a':123, 'b':456, 'c':[{'d':999}, {'d':111}]}
-
-ret = jsonpath.jsonpath(a, '$.c[*].d')
-print(ret)
-print(a)
+CaseUtil().harToJson('1.har')
